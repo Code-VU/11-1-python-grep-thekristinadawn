d0ef8af5f701783284ae3b97d6078e357241890b
Kristina Palmer <Kristina.Palmer@vu.com>
2022-04-23 13:02:32 -0500

    1st Attempt

--- grep.py
+++ grep.py
@@ -5,8 +5,19 @@ import sys
 
 def countpatterninfile():
     regular_expression = input("Enter a regular expression: ")
+    count = 0
+    file_name = open('mbox-long.txt','r')
+    for line in file_name: 
+        line = line.rstrip()
+        if re.search(regular_expression, line): 
+            count += 1
+            #print(count)
+            #print(line)
+    print(f'mbox.txt had {count} lines that matched',regular_expression)
+
 
 if __name__ == '__main__':
+    
     # this is called a main method
     # This is another way of telling python THIS IS WHERE YOU SHOULD START RUNNING
     # When this is included, python will begin with the code in this block first
